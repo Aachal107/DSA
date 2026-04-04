9989cac09d54cb3b3b708eb58824ac589828f8c4
Aachal Arvind Singh <136174448+Aachal107@users.noreply.github.com>
2026-04-04 16:46:10 +0530

    added two sum solution

---
+++ 1.Two sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self,nums, target):
+       
+        hash_map={}
+
+        for i ,num in enumerate(nums): #enumerate return index and number
+            if target - num in hash_map:
+                return [hash_map[target-num],i]
+            hash_map[num]=i
+
+        return []
+
+nums=[2,7,11,15]
+target=9
+
+sol=Solution()
+print(sol.twoSum(nums,target))    
+
+        
