1c770940bdaf00c7178a147cb4afe9ef1640a555
Aachal Arvind Singh <136174448+Aachal107@users.noreply.github.com>
2026-04-08 23:05:38 +0530

    monotonic array or not

---
+++ monotonicArray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        inc=True
+        dec=True
+        for i in range(1,len(nums)):
+            if nums[i] < nums[i-1]:
+                inc=False
+            if nums[i] > nums[i-1]:
+                dec=False
+
+        return inc or dec            
+        
+nums=[1,2,2,3]
+sol=Solution()
+print(sol.isMonotonic(nums))    
