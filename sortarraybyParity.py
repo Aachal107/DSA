16711b56486bdf21a332f20b53ba7125333778d3
Aachal Arvind Singh <136174448+Aachal107@users.noreply.github.com>
2026-04-13 23:23:53 +0530

    Sort Array by parity

---
+++ sortarraybyParity.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        l=0
+        for r in range(len(nums)):
+            if nums[r] % 2==0:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+        return nums 
+nums=[3,1,2,4]
+sol=Solution()
+print(sol.sortArrayByParity(nums))    
